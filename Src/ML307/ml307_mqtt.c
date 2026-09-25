c3aa778e326fbe746e75aa4f6b98e76b106596c5
杨天宇 <wshyshb@gmail.com>
2026-09-25 12:05:29 +0800

    Build ML307 MQTT SSL configuration command

--- Src/ML307/ml307_mqtt.c
+++ Src/ML307/ml307_mqtt.c
@@ -51,6 +51,19 @@ ML307_Result ML307_MqttBuildCleanSession(char *output, size_t output_size,
                     (unsigned int)connect_id, (unsigned int)clean_session);
 }
 
+ML307_Result ML307_MqttBuildSslConfig(char *output, size_t output_size,
+                                      uint8_t connect_id, uint8_t ssl_enable,
+                                      uint8_t ssl_id)
+{
+  if ((connect_id > 5U) || (ssl_enable > 1U) || (ssl_id > 5U)) {
+    return ML307_RESULT_INVALID_VALUE;
+  }
+  return MqttFormat(output, output_size,
+                    "AT+MQTTCFG=\"ssl\",%u,%u,%u\r\n",
+                    (unsigned int)connect_id, (unsigned int)ssl_enable,
+                    (unsigned int)ssl_id);
+}
+
 ML307_Result ML307_MqttBuildConnect(char *output, size_t output_size,
                                     uint8_t connect_id, const char *host,
                                     uint16_t port, const char *client_id,
